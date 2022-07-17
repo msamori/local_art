d1d5d4ed5710c256787dd6c984badb9e1630d1fc
msamori <mahmoud.samori@gmail.com>
2022-07-16 21:40:16 -0400

    upload behaves like homescreen

--- client/screens/Upload.tsx
+++ client/screens/Upload.tsx
@@ -1,20 +1,22 @@
 import * as MediaLibrary from "expo-media-library";
-import { useEffect, useState } from "react";
-import { Dimensions, Image, ScrollView, StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Dimensions, Image, Pressable, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+import AppIntroSlider from "react-native-app-intro-slider";
 import { TopBar } from "../components";
+import { Context } from "../../utils";
 
 function Upload(props){
 
+  const { currentLocation, locationPermission, mapRegion, setMapRegion } = useContext(Context);
+
   const [pics, setPics] = useState([]);
-  const [singlePic, setSinglePic] = useState({ latitude: 0, longitude: 0 });
   const [loadingPics, setLoadingPics] = useState(true);
 
   useEffect(() => {
     getPhotos();
     return () => {
       setPics([]);
-      setSinglePic({ latitude: 0, longitude: 0 });
     };
   }, []);
 
@@ -47,11 +49,36 @@ function Upload(props){
     }
 
     setPics([...pics, ...picArray]);
-    setSinglePic(picArray[0]);
     setLoadingPics(false);
   }
 
   console.log("test pic", pics[0]);
+
+  function shiftMap(item){
+    const newRegion = {
+      latitude: item.latitude,
+      longitude: item.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01
+    }
+    setMapRegion(newRegion)
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+      <Pressable onLongPress={()=>{
+        shiftMap(item);
+      }}
+      >
+      <Image source={{ uri: item.uri }} style={styles.selectedPic} />
+      </Pressable>
+    </View>
+
+  )};
+
+  const keyExtractor = (item) => item.uri;
+  const sliderEl = useRef(null);
 
   if (loadingPics) {
     return <></>;
@@ -60,9 +87,40 @@ function Upload(props){
   return (
     <View style={styles.container}>
       <TopBar navigation={props.navigation} />
-      <Image style={styles.selectedPic} source={{uri: pics[1].uri}}/>
+      <AppIntroSlider
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        data={pics}
+        ref={sliderEl}
+      />
+      <MapView style={styles.map} region={mapRegion}>
+        {pics.map((pic) => {
+          return (
+            <MapView.Marker
+            pinColor={"purple"}
+              key={pic.uri}
+              coordinate={{
+                latitude: pic.latitude,
+                longitude: pic.longitude,
+              }}
+
+            />
+          );
+        })}
+        { locationPermission ? (
+          <MapView.Marker
+          pinColor={"blue"}
+          coordinate={{
+            latitude: currentLocation.coords.latitude,
+            longitude: currentLocation.coords.longitude,
+          }}
+        />
+        ) : (
+          <></>
+        )}
+      </MapView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -70,11 +128,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "black",
   },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height * 0.4,
+    position: "absolute",
+    bottom: 0,
+  },
   selectedPic: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height * 0.6,
     top: Dimensions.get("window").height * 0.10,
-  }
+  },
+  slide: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
+  },
 });
 
 export { Upload };
